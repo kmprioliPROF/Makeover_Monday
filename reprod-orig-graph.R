9012a5c356636c0d2b21bd80e713ccd1e9b708a7
Katherine M. Prioli <katherine.prioli@rutgers.edu>
2018-09-16 19:50:50 -0400

    Revisions to first graph reproduction

--- reprod-orig-graph.R
+++ reprod-orig-graph.R
@@ -11,7 +11,7 @@ library(tidyverse)
 #### Read in and subset data ----
 
 incomedata <- read_csv("ACS_16_1YR_S1901_with_ann.csv")
-incomedata <- incomedata %>% select("GEO.display-label",
+incomedata <- incomedata %>% select("GEO.display-label",     # Subset to only the variables of interest
                                     "HC01_EST_VC01",
                                     "HC01_EST_VC02",
                                     "HC01_EST_VC03",
@@ -24,22 +24,22 @@ incomedata <- incomedata %>% select("GEO.display-label",
                                     "HC01_EST_VC10",
                                     "HC01_EST_VC11")
 
-colnames(incomedata) <- c("state",                   # GEO.display-label
-                          "tot_households",          # HC01_EST_VC01
-                          "Less than $10,000",       # HC01_EST_VC02
-                          "$10,000 to $14,999",      # HC01_EST_VC03
-                          "$15,000 to $24,999",      # HC01_EST_VC04
-                          "$25,000 to $34,999",      # HC01_EST_VC05
-                          "$35,000 to $49,999",      # HC01_EST_VC06
-                          "$50,000 to $74,999",      # HC01_EST_VC07
-                          "$75,000 to $99,999",      # HC01_EST_VC08
-                          "$100,000 to $149,999",    # HC01_EST_VC09
-                          "$150,000 to $199,999",    # HC01_EST_VC10
-                          "$200,000 or more")        # HC01_EST_VC11
+colnames(incomedata) <- c("state",                           # GEO.display-label
+                          "tot_households",                  # HC01_EST_VC01
+                          "Less than $10,000",               # HC01_EST_VC02
+                          "$10,000 to $14,999",              # HC01_EST_VC03
+                          "$15,000 to $24,999",              # HC01_EST_VC04
+                          "$25,000 to $34,999",              # HC01_EST_VC05
+                          "$35,000 to $49,999",              # HC01_EST_VC06
+                          "$50,000 to $74,999",              # HC01_EST_VC07
+                          "$75,000 to $99,999",              # HC01_EST_VC08
+                          "$100,000 to $149,999",            # HC01_EST_VC09
+                          "$150,000 to $199,999",            # HC01_EST_VC10
+                          "$200,000 or more")                # HC01_EST_VC11
 
 incomedata <- incomedata %>% 
   slice(2:n()) %>% 
-  filter(state != "Puerto Rico" & state != "District of Columbia")  # Omitting DC and PR per original graph
+  filter(state != "Puerto Rico" & state != "District of Columbia")       # Omit DC and PR per original graph
 #View(incomedata)
 
 
@@ -64,46 +64,63 @@ incometbl <- incomegat %>%
   mutate(pct = as.numeric(pct)) %>% 
   mutate(n_households = round((pct / 100) * tot_households, digits = 0)) %>% 
   mutate(incomecat = case_when(
-    incomelev == "Less than $10,000" ~ 1,
-    incomelev == "$10,000 to $14,999" ~ 1,
-    incomelev == "$15,000 to $24,999" ~ 1,
-    incomelev == "$25,000 to $34,999" ~ 2,
-    incomelev == "$35,000 to $49,999" ~ 2,
-    incomelev == "$50,000 to $74,999" ~ 3,
-    incomelev == "$75,000 to $99,999" ~ 4,
-    incomelev == "$100,000 to $149,999" ~ 5,
-    incomelev == "$150,000 to $199,999" ~ 6,
-    incomelev == "$200,000 or more" ~ 6)) %>% 
+    incomelev == "Less than $10,000" ~ "<$25K",
+    incomelev == "$10,000 to $14,999" ~ "<$25K",
+    incomelev == "$15,000 to $24,999" ~ "<$25K",
+    incomelev == "$25,000 to $34,999" ~ "$25K-$50K",
+    incomelev == "$35,000 to $49,999" ~ "$25K-$50K",
+    incomelev == "$50,000 to $74,999" ~ "$51K-$75K",
+    incomelev == "$75,000 to $99,999" ~ "$76K-$100K",
+    incomelev == "$100,000 to $149,999" ~ "$101K-$150K",
+    incomelev == "$150,000 to $199,999" ~ ">$150K",
+    incomelev == "$200,000 or more" ~ ">$150K")) %>% 
+  mutate(incomecat = factor(incomecat,
+                        levels = c("<$25K", 
+                                   "$25K-$50K", 
+                                   "$51K-$75K", 
+                                   "$76K-$100K", 
+                                   "$101K-$150K", 
+                                   ">$150K"))) %>%
   group_by(state, incomecat) %>% 
   mutate(catpct = sum(pct)) %>% 
-  select(state, incomecat, catpct) %>% 
-  distinct(state, incomecat, catpct)
+  select(state, catpct, incomecat) %>% 
+  distinct(state, catpct, incomecat) %>% 
+  arrange(desc(incomecat), catpct)
 #View(incometbl)
 
-#incomesubset <- incometbl %>% filter(state %in% c("West Virginia", "Pennsylvania", "New Jersey"))
 
+#### Recreate the original graph ----
 
-#### Recreating the original graph ----
-
-orig_plot <- ggplot(incometbl, aes(x = state, y = catpct / 100, fill = incomecat, label = catpct)) +
-  coord_flip() +
-  #ylim(0, 100) +
+reprod_plot <- ggplot(incometbl, aes(x = state, y = catpct / 100, 
+                                     fill = incomecat, label = catpct)) +
+  coord_flip() +                                                                    # Put states on vertical axis
   geom_bar(stat = "identity", aes(y = catpct * 100), position = "fill") +
-  geom_text(aes(label = paste0(catpct,"%")), size = 2, color = "white", hjust = 0.5, position = position_stack(vjust = 0.5)) +
+  scale_fill_manual(values = c(                                                     # Match color scheme of original graph
+    "<$25K" = "#D62728",
+    "$25K-$50K" = "#FF7F03",
+    "$51K-$75K" = "#FFD500",
+    "$76K-$100K" = "#9467BD",
+    "$101K-$150K" = "#1F77B4",
+    ">$150K" = "#6FAA12")) +
+  geom_text(aes(label = paste0(catpct,"%")),                                        # Label stacked bars
+            size = 2, color = "white", fontface = "bold", 
+            hjust = 0.5, position = position_stack(vjust = 0.5)) +
+  scale_y_reverse() +                                                               # Ensure <$25K category on left
   theme(axis.text.x = element_blank(),
         axis.ticks = element_blank(),
         axis.title = element_blank(),
         panel.background = element_blank(),
         legend.position = "top",
-        aspect.ratio = 1.61) +
+        legend.title = element_blank(),
+        legend.key.width = unit(0.5, "cm"),
+        legend.key.height = unit(0.5, "cm")) +
+  guides(fill = guide_legend(nrow = 1)) +                                           # Force all legend elements into one row
   ggtitle("HOUSEHOLD INCOME DISTRIBUTION IN USA BY STATE")
-orig_plot
+reprod_plot
 
-#ggsave("orig_plot.png", height = 15, width = 9.1, units = "in")
+#ggsave("reprod_plot.png", height = 15, width = 9.1, units = "in")
 
 
-#### NEXT STEPS:
-     # Discretize the income categories and apply a custom color scale
-     # Properly format the labels (they're in the thousands right now)
-     # Order the graph by increasing income category 6
-     # Apply nice formatting to legend
+#### NEXT STEPS ----
+     # Order states such that <$200K category increases from top to bottom
+     # Cosmetic improvements as needed
